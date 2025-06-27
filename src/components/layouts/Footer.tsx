dfe886112394702f31c2305c55daf7e866894e78
quy12022002 <ban12022002@gmail.com>
2025-06-27 18:56:29 +0700

    update Footer.tsx

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -8,16 +8,36 @@ const Footer = () => {
       <Container className="footer-container">
         <Nav className="list-unstyled d-flex flex-column flex-md-row justify-content-between align-items-center mb-0">
           <div className="d-flex gap-3 ">
-            <Nav.Link className="footer-icon">
+            <Nav.Link
+              href="https://github.com/quy12022002"
+              className="footer-icon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaGithub />
             </Nav.Link>
-            <Nav.Link className="footer-icon">
+            <Nav.Link
+              href="https://www.linkedin.com/in/quy120202/"
+              className="footer-icon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaLinkedinIn />
             </Nav.Link>
-            <Nav.Link className="footer-icon">
+            <Nav.Link
+              href="https://www.facebook.com/quy120202/"
+              className="footer-icon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaFacebookF />
             </Nav.Link>
-            <Nav.Link className="footer-icon">
+            <Nav.Link
+              href="https://x.com/quy120202"
+              className="footer-icon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <BsTwitterX />
             </Nav.Link>
           </div>
