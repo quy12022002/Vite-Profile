f054632604e01b34f2e068473af768d8f95cf612
quy12022002 <ban12022002@gmail.com>
2025-07-01 00:01:45 +0700

    fix: update Skills.tsx

--- src/components/layouts/Skills.tsx
+++ src/components/layouts/Skills.tsx
@@ -1,8 +1,9 @@
 import { Col, Container, Row } from "react-bootstrap";
 import { DiCode } from "react-icons/di";
-import { GoDatabase, GoPeople } from "react-icons/go";
-import { LuBrain } from "react-icons/lu";
+import { GoDatabase } from "react-icons/go";
+import { LiaLaptopCodeSolid } from "react-icons/lia";
 import { RiServerLine } from "react-icons/ri";
+import { TbApps } from "react-icons/tb";
 import { VscSymbolColor } from "react-icons/vsc";
 
 const Skills = () => {
@@ -18,6 +19,27 @@ const Skills = () => {
           <Col xs={12} sm={6} md={6} lg={4} xl={4}>
             <div className="card p-4 h-100">
               <div className="d-flex align-items-center mb-3">
+                <div className="icon-circle icon-ai me-3">
+                  <LiaLaptopCodeSolid />
+                </div>
+                <h5 className="fw-semibold mb-0 text-white">
+                  Programming Languages
+                </h5>
+              </div>
+              <div>
+                <span className="badge-custom">C#</span>
+                <span className="badge-custom">HTML5/CSS</span>
+                <span className="badge-custom">PHP</span>
+                <span className="badge-custom">Java</span>
+                <span className="badge-custom">JavaScript</span>
+                <span className="badge-custom">TypeScript</span>
+                <span className="badge-custom">Python basic</span>
+              </div>
+            </div>
+          </Col>
+          <Col xs={12} sm={6} md={6} lg={4} xl={4}>
+            <div className="card p-4 h-100">
+              <div className="d-flex align-items-center mb-3">
                 <div className="icon-circle icon-backend me-3">
                   <RiServerLine />
                 </div>
@@ -26,10 +48,9 @@ const Skills = () => {
                 </h5>
               </div>
               <div>
-                <span className="badge-custom">PHP</span>
-                <span className="badge-custom">Node.js</span>
-                <span className="badge-custom">Express.js</span>
-                <span className="badge-custom">RESTful APIs</span>
+                <span className="badge-custom">Node.js with Express</span>
+                <span className="badge-custom">Laravel basic</span>
+                <span className="badge-custom">RESTful APIs basic</span>
               </div>
             </div>
           </Col>
@@ -44,12 +65,25 @@ const Skills = () => {
                 </h5>
               </div>
               <div>
-                <span className="badge-custom">HTML/CSS</span>
                 <span className="badge-custom">React</span>
-                <span className="badge-custom">JavaScript</span>
-                <span className="badge-custom">TypeScript</span>
                 <span className="badge-custom">Bootstrap</span>
-                <span className="badge-custom">SASS</span>
+                <span className="badge-custom">SASS/SCSS</span>
+                <span className="badge-custom">Tailwind CSS basic</span>
+              </div>
+            </div>
+          </Col>
+          <Col xs={12} sm={6} md={6} lg={4} xl={4}>
+            <div className="card p-4 h-100">
+              <div className="d-flex align-items-center mb-3">
+                <div className="icon-circle icon-database me-3">
+                  <GoDatabase />
+                </div>
+                <h5 className="fw-semibold mb-0 text-white">Database</h5>
+              </div>
+              <div>
+                <span className="badge-custom">MySQL</span>
+                <span className="badge-custom">Firebase basic</span>
+                <span className="badge-custom">MongoDB basic</span>
               </div>
             </div>
           </Col>
@@ -70,45 +104,15 @@ const Skills = () => {
           <Col xs={12} sm={6} md={6} lg={4} xl={4}>
             <div className="card p-4 h-100">
               <div className="d-flex align-items-center mb-3">
-                <div className="icon-circle icon-database me-3">
-                  <GoDatabase />
-                </div>
-                <h5 className="fw-semibold mb-0 text-white">Database</h5>
-              </div>
-              <div>
-                <span className="badge-custom">MongoDB</span>
-                <span className="badge-custom">MySQL</span>
-              </div>
-            </div>
-          </Col>
-          <Col xs={12} sm={6} md={6} lg={4} xl={4}>
-            <div className="card p-4 h-100">
-              <div className="d-flex align-items-center mb-3">
-                <div className="icon-circle icon-ai me-3">
-                  <LuBrain />
-                </div>
-                <h5 className="fw-semibold mb-0 text-white">AI & ML</h5>
-              </div>
-              <div>
-                <span className="badge-custom">ChatGPT</span>
-                <span className="badge-custom">Gemini</span>
-                <span className="badge-custom">Deep seek</span>
-                <span className="badge-custom">Copilot</span>
-              </div>
-            </div>
-          </Col>
-          <Col xs={12} sm={6} md={6} lg={4} xl={4}>
-            <div className="card p-4 h-100">
-              <div className="d-flex align-items-center mb-3">
                 <div className="icon-circle icon-softskills me-3">
-                  <GoPeople />
+                  <TbApps />
                 </div>
                 <h5 className="fw-semibold mb-0 text-white">Other Skills</h5>
               </div>
               <div>
+                <span className="badge-custom">Git</span>
                 <span className="badge-custom">Docker</span>
                 <span className="badge-custom">AWS</span>
-                <span className="badge-custom">Github</span>
                 <span className="badge-custom">Postman</span>
               </div>
             </div>
