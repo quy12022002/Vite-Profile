c30b35e9aa9986ec2a5e9cd3ae38f93087727967
quy12022002 <ban12022002@gmail.com>
2025-07-01 17:54:37 +0700

    fix: fix component content.tsx hidden in background

--- src/components/layouts/Template.tsx
+++ src/components/layouts/Template.tsx
@@ -5,7 +5,7 @@ import Header from "./Header";
 const Template = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div>
+      <div id="template-container">
         <Background />
         <Header />
         <main className="">{children}</main>
